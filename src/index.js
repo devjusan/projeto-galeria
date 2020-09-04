af4b3fbe9f15c84290633bcc44a801744dedfb05
jnrplay <jnrplay@hotmail.com>
2020-09-03 22:23:33 -0300

    starting

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Arquivos SASS
+import './scss/index.scss'
+
+// Dependencias
+
+import 'jquery'
+import 'bootstrap'
+
+// arquivos JS
+
+import './js/core/includes'
+import './js/plugins/cityButtons'
